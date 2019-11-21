863cc6df226df608c160111fca59b4de5d6a3c83
Dan McKinney <3dhdaniel@gmail.com>
2019-11-21 10:55:22 -0500

    thirsty thursday

--- devcamper-api/models/User.js
+++ devcamper-api/models/User.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const crypto = require('crypto')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -30,7 +30,7 @@ const UserSchema = new mongoose.Schema({
         select: false
     },
     resetPasswordToken: String,
-    resetPasswordExpired: Date,
+    resetPasswordExpire: Date,
     createdAt: {
         type: Date,
         default: Date.now
@@ -39,6 +39,9 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        next()
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
@@ -54,5 +57,21 @@ UserSchema.methods.getSignedJwtToken = function() {
 UserSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 }
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+    const resetToken = crypto.randomBytes(20).toString('hex');
+    // hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+        // set expire
+        this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+        return resetToken;
+}
+
+
 
 module.exports = mongoose.model('User', UserSchema);
